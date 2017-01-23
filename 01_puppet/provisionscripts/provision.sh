4f6ac4cb524ebea6b1d1cbdd3db0edebed32789f
chris vugrinec <chris@air-chris.home>
2017-01-23 11:19:54 +0100

    added to docker group

--- 01_puppet/provisionscripts/provision.sh
+++ 01_puppet/provisionscripts/provision.sh
@@ -3,4 +3,6 @@ password="HelloWorld123!"
 apt-get update
 apt -y  install docker.io
 /usr/sbin/useradd -p `openssl passwd -1 $password` vsts
+# adding user vsts to docker group
+usermod -aG docker vsts
 
