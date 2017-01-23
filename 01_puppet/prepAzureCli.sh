29e8fd1b9f80cccd9eb2ca89c1e0b847658fd569
chris vugrinec <chris@air-chris.home>
2017-01-23 23:19:52 +0100

    added missing

--- 01_puppet/prepAzureCli.sh
+++ 01_puppet/prepAzureCli.sh
@@ -11,7 +11,8 @@ apt-get update
 apt-get -y install git
 #apt-get -y install nodejs-legacy
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+apt-get install -y nodejs
+apt-get install -y build-essential
 apt-get -y install npm
 apt-get -y install jq
 npm -g install azure-cli
