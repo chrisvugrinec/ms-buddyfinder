6fafe9a6c2582097dc0b67ffb4aa17026bb67175
root <root@puppetmaster.nkxmqvv5j4burku1v0q3leqeyf.ax.internal.cloudapp.net>
2017-01-23 10:04:59 +0000

    added helper util

---
+++ 01_puppet/puppet-templates/generateapp.sh
@@ -0,0 +1,10 @@
+#!/bin/bas
+#
+# small helper app to guide your through configuring your template
+#
+echo "what is the name of the resourcegroup of your puppetmaster"
+read rg
+sed  -i 's/msbf-demo/'$rg'/g' buildagent-custom.pp
+echo "what is the name of your buildagent, ps think of a unique name (DNS)"
+read name
+sed  -i 's/buildagent1/'$name'/g' buildagent-custom.pp
