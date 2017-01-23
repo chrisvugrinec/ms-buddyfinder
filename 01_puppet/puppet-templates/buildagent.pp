65870c98c002c2c3a1732dc62eb6a833dae293bf
root <root@puppetmaster.zjyqmph5k5ve3hgjoifdctaqld.ax.internal.cloudapp.net>
2017-01-23 08:34:22 +0000

    added buildagent.pp

--- 01_puppet/puppet-templates/buildagent.pp
+++
@@ -1,9 +0,0 @@
-azure_vm { 'buildagent-no2':
-  ensure         => present,
-  location       => 'westeurope',
-  image          => 'canonical:ubuntuserver:14.04.2-LTS:latest',
-  user           => 'chris',
-  password       => 'HelloWorld123!',
-  size           => 'Standard_A0',
-  resource_group => 'demo-infra',
-}
---
+++ 01_puppet/puppet-templates/test.sh
@@ -0,0 +1,2 @@
+#azure vm extension set msbf-demo sample1 CustomScript Microsoft.Azure.Extensions 2.0 --auto-upgrade-minor-version --public-config-path ./test.json
+azure vm extension set msbf-demo sample11 CustomScript Microsoft.Azure.Extensions 2.0 --auto-upgrade-minor-version --public-config-path ./test2.json
