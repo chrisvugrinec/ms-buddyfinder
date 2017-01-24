9458211d639061cbc0a50beec64e4576e8ff8c5c
chris vugrinec <chris@air-chris.home>
2017-01-24 09:54:49 +0100

    added stuff for storage account

--- 01_puppet/puppet-templates/generateapp.sh
+++ 01_puppet/puppet-templates/generateapp.sh
@@ -8,3 +8,7 @@ sed  -i 's/msbf-demo/'$rg'/g' buildagent-custom.pp
 echo "what is the name of your buildagent, ps think of a unique name (DNS)"
 read name
 sed  -i 's/buildagent1/'$name'/g' buildagent-custom.pp
+echo "unique name of your storageaccount (only alfanumeric signs)"
+read sa
+sed  -i 's/cvugrinecdemo/'$sa'/g' buildagent-custom.pp
+
