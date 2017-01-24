2062977bf87f5a3e38b92b06d858d0724b4b82cc
chris vugrinec <chris@air-chris.home>
2017-01-24 20:47:37 +0100

    added dockerregistry module and bfix redis

--- 02_azure-infra/azure_mplace_dockerregistry/manifests/init.pp
+++ 02_azure-infra/azure_mplace_dockerregistry/manifests/init.pp
@@ -1,48 +1,39 @@
-# Class: azure_mplace_dockerregistry
-# ===========================
-#
-# Full description of class azure_mplace_dockerregistry here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'azure_mplace_dockerregistry':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2017 Your name here, unless otherwise noted.
-#
 class azure_mplace_dockerregistry {
 
+  $resourcegroup= hiera("azure-dockerregistry.resourcegroup")
+  $dockerregistryname= hiera("azure-dockerregistry.name")
+
+  file { '/opt/puppet':
+    ensure => 'directory',
+  }
+
+  file { "/opt/puppet/install_dregistry.sh":
+     mode => '0750',
+     owner => 'root',
+     group => 'root',
+     source => "puppet:///modules/azure_mplace_dockerregistry/install_dregistry.sh",
+  }
+
+  file { "/opt/puppet/dregistry.json":
+     mode => '0750',
+     owner => 'root',
+     group => 'root',
+     source => "puppet:///modules/azure_mplace_dockerregistry/dregistry.json",
+  }
+
+  file { "/opt/puppet/certificate.pem":
+     mode => '0750',
+     owner => 'root',
+     group => 'root',
+     source => "/root/ms-buddyfinder/01_puppet/certificate.pem",
+  }
+
+  exec { "install_dockerregistry":
+    command => "install_dregistry.sh" $resourcegroup $dockerregistryname",
+    cwd => "/opt/puppet/",
+    path => '/opt/puppet/:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin',
+    refreshonly => true,
+    timeout     => 18000,
+  }
 
 }
--- 02_azure-infra/azure_mplace_redis/files/install_redis.sh
+++ 02_azure-infra/azure_mplace_redis/files/install_redis.sh
@@ -2,4 +2,4 @@
 resourcegroup=$1
 rcachename=$2
 sname=$3
-azure group deployment create msbf-demo1 -p "{\"redisCacheName\":{\"value\":\""$rcachename""\"},\"existingDiagnosticsStorageAccountName\":{\"value\":\""$sname""\"}}" --template-file redis.json 
+azure group deployment create $resourcegroup -p "{\"redisCacheName\":{\"value\":\""$rcachename""\"},\"existingDiagnosticsStorageAccountName\":{\"value\":\""$sname""\"}}" --template-file redis.json 
