9fdb332be534881facce55061f572d8c69636ef5
root <root@puppetmaster.ctme3ehuzraudbviwyjpapcsnh.ax.internal.cloudapp.net>
2017-01-25 02:04:54 +0000

    cleaned

--- 02_azure-infra/azure_acs_kubernetes/manifests/init.pp
+++ 02_azure-infra/azure_acs_kubernetes/manifests/init.pp
@@ -16,8 +16,14 @@ class azure_acs_kubernetes {
   ssh_keygen { 'root':
     type => 'rsa'
   }->
+  file { '/opt/puppet/create-kubecluster.sh':
+    mode   => '0750',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/azure_acs_kubernetes/create-kubecluster.sh',
+  }->
   exec { 'install_acs_kube':
-    command     => 'install_dregistry.sh $resourcegroup $dockerregistryname',
+    command     => 'install_dregistry.sh $resourcegroup $numberofnodes',
     cwd         => '/opt/puppet/',
     path        => '/opt/puppet/:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin',
     refreshonly => true,
