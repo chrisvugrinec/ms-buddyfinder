a06017cc53c9d2155b3471a70196cecddc8c11a9
chris vugrinec <chris@air-chris.home>
2017-01-26 20:58:34 +0100

    cleanup

--- 04_vsts/code/ms-buddyfinder-nginx/src/index.js
+++ 04_vsts/code/ms-buddyfinder-nginx/src/index.js
@@ -4,16 +4,15 @@ var io = require('socket.io')(http);
 var redis = require("redis");
 var serveStatic = require('serve-static');
 
-var subscriber= redis.createClient(6380,'datalinks.redis.cache.windows.net', {auth_pass: 'PASS', tls: {servername: 'datalinks.redis.cache.windows.net'}});
-var bc_subscriber= redis.createClient(6380,'datalinks.redis.cache.windows.net', {auth_pass: 'PASS', tls: {servername: 'datalinks.redis.cache.windows.net'}});
+var subscriber= redis.createClient(6380,process.env.REDIS_URL, {auth_pass:process.env.REDIS_PASSWORD, tls: {servername: process.env.REDIS_URL}});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 
-bc_subscriber.subscribe("MSBUDDYFINDER_BROADCAST");
-bc_subscriber.on("message", function(channel, message) {
+subscriber.subscribe("MSBUDDYFINDER_BROADCAST");
+subscriber.on("message", function(channel, message) {
     io.emit('MSBUDDYFINDER_BROADCAST', message);
 });
 
