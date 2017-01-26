c37c14b993e81ecf1794abc260aa3dfdd31201f7
chris vugrinec <chris@air-chris.home>
2017-01-26 12:26:20 +0100

    added provioning

--- 01_puppet/provisionscripts/provision-buildagent.sh
+++ 01_puppet/provisionscripts/provision-buildagent.sh
@@ -2,8 +2,12 @@
 password="HelloWorld123!"
 apt-get update
 apt -y  install docker.io
-/usr/sbin/useradd -p `openssl passwd -1 $password` vsts
+/usr/sbin/useradd -m -d /vsts -p `openssl passwd -1 $password` vsts
+# fix needed for vsts on ubuntu 16.04
 apt-get install -y libunwind8 libcurl3
 apt-get install -y libunwind8 libcurl3 libicu52
 # adding user vsts to docker group
 usermod -aG docker vsts
+sudo -H -u vsts bash -c '/bin/mkdir /vsts/bagent'
+sudo -H -u vsts bash -c 'cd /vsts/bagent && wget https://github.com/Microsoft/vsts-agent/releases/download/v2.111.1/vsts-agent-ubuntu.16.04-x64-2.111.1.tar.gz' 
+sudo -H -u vsts bash -c 'tar -zxvf /vsts/bagent/vsts-agent-ubuntu.16.04-x64-2.111.1.tar.gz -C /vsts/bagent'
