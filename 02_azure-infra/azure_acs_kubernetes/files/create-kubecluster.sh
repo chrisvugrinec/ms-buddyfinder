fc183294c83fac67af799142fc9efe8b16db0e4e
root <root@puppetmaster.haq5zuxiuwle5na4n2aml4hejd.ax.internal.cloudapp.net>
2017-01-26 06:18:57 +0000

    restructure

--- 02_azure-infra/azure_acs_kubernetes/files/create-kubecluster.sh
+++ 02_azure-infra/azure_acs_kubernetes/files/create-kubecluster.sh
@@ -1,51 +1,10 @@
 #!/bin/bash
 
-rgroupname=$1
-numberofnodes=$2
+resourcegroup=$1
+id=$(cat /opt/puppet/id)
 
-# Gather stuff needed for keys in template
-tmpkey=`echo $(cat ~/.ssh/id_rsa.pub)`
-sshKey=$(echo "$tmpkey" | sed 's/\//\\\//g')
-clientid=$(cat /etc/puppetlabs/puppet/azure.conf | grep client_id | sed 's/[\"]//g' | sed 's/^.*[:] //')
-clientsecret=$(cat /etc/puppetlabs/puppet/azure.conf | grep client_secret | sed 's/[\"]//g' | sed 's/^.*[:] //')
-id=$(uuid)
+azure group create --name=$resourcegroup --location="westeurope"
+azure group deployment create --name=$id --resource-group=$resourcegroup --template-file="azuredeploy.json" --parameters-file="azuredeploy.parameters.json"
 
-if [ -d ~/go/src/github.com/Azure/acs-engine/ ]
-then 
-  rm -rf ~/go/src/github.com/Azure/acs-engine/
-fi
-
-export GOPATH=/root/go
-go get github.com/Azure/acs-engine
-go get all
-cd $GOPATH/src/github.com/Azure/acs-engine
-go build
-
-cp ~/go/src/github.com/Azure/acs-engine/examples/kubernetes.json /opt/puppet/acs-kube-template.json
-cd /opt/puppet
-sed -in "s/keyData\": \"\"/keyData\": \"$sshKey\"/g" acs-kube-template.json
-sed -in "s/servicePrincipalClientID\": \"\"/servicePrincipalClientID\": \"$clientid\"/g" acs-kube-template.json
-sed -in "s/servicePrincipalClientSecret\": \"\"/servicePrincipalClientSecret\": \"$clientsecret\"/g" acs-kube-template.json
-sed -in "s/dnsPrefix\": \"\"/dnsPrefix\": \"a${id}z\"/g" acs-kube-template.json
-sed -in "s/\"count\": 3,/\"count\": $numberofnodes,/" acs-kube-template.json
-
-cd ~/go/src/github.com/Azure/acs-engine
-./acs-engine /opt/puppet/acs-kube-template.json
-cp _output/Kubernetes*/azuredeploy* /opt/puppet/
-cd /opt/puppet
-
-azure group create \
-    --name=$rgroupname \
-    --location="westeurope"
-
-azure group deployment create \
-    --name=$id \
-    --resource-group="$rgroupname" \
-    --template-file="azuredeploy.json" \
-    --parameters-file="azuredeploy.parameters.json"
-
-if [ ! -d ~/.kube ]
-then
-  mkdir ~/.kube 
-fi
 echo "scp azureuser@a${id}z.westeurope.cloudapp.azure.com:~/.kube/config ~/.kube/config" >~/kube-msg.txt
+
--- 02_azure-infra/azure_acs_kubernetes/manifests/init.pp
+++ 02_azure-infra/azure_acs_kubernetes/manifests/init.pp
@@ -9,6 +9,9 @@ class azure_acs_kubernetes {
   file { '/root/go':
     ensure => 'directory',
   }
+  file { '/root/kube':
+    ensure => 'directory',
+  }
   package { 'gccgo-go':
     ensure  => installed,
     require => Exec['apt-update'],
@@ -19,19 +22,32 @@ class azure_acs_kubernetes {
   ssh_keygen { 'root':
     type => 'rsa'
   }->
+  file { '/opt/puppet/config-kubecluster.sh':
+    mode   => '0750',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/azure_acs_kubernetes/config-kubecluster.sh',
+  }->
   file { '/opt/puppet/create-kubecluster.sh':
     mode   => '0750',
     owner  => 'root',
     group  => 'root',
     source => 'puppet:///modules/azure_acs_kubernetes/create-kubecluster.sh',
   }->
-  exec { 'install_acs_kube':
-    command     => "create-kubecluster.sh $resourcegroup $numberofnodes",
+  exec { 'config_acs_kube':
+    command     => "config-kubecluster.sh $numberofnodes",
     cwd         => '/opt/puppet/',
     path        => '/root/go/bin/:/opt/puppet/:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin',
     user        => "root",
     refreshonly => false,
-    timeout     => 18000,
+    timeout     => 1800,
+  }->
+  exec { 'create_acs_kube':
+    command     => "create-kubecluster.sh $resourcegroup",
+    cwd         => '/opt/puppet/',
+    path        => '/root/go/bin/:/opt/puppet/:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin',
+    user        => "root",
+    refreshonly => false,
+    timeout     => 1800,
   }
-
 }
