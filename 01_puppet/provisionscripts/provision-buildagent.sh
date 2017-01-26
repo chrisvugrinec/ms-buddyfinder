d726a2a8c034d85ad68e2d5b9e0041b492b3ad7b
chris vugrinec <chris@air-chris.home>
2017-01-26 22:02:21 +0100

    added .kube

--- 01_puppet/provisionscripts/provision-buildagent.sh
+++ 01_puppet/provisionscripts/provision-buildagent.sh
@@ -6,9 +6,10 @@ apt -y  install docker.io
 # fix needed for vsts on ubuntu 16.04
 apt-get install -y libunwind8 libcurl3
 apt-get install -y libunwind8 libcurl3 libicu52
-sudo -H -u vsts bash -c 'ssh-keygen -t rsa -f  ~/.ssh/id_rsa -P ""'
 # adding user vsts to docker group
 usermod -aG docker vsts
+sudo -H -u vsts bash -c 'ssh-keygen -t rsa -f  ~/.ssh/id_rsa -P ""'
+sudo -H -u vsts bash -c 'mkdir ~/.kube'
 sudo -H -u vsts bash -c '/bin/mkdir /vsts/bagent'
 sudo -H -u vsts bash -c 'cd /vsts/bagent && wget https://github.com/Microsoft/vsts-agent/releases/download/v2.111.1/vsts-agent-ubuntu.16.04-x64-2.111.1.tar.gz' 
 sudo -H -u vsts bash -c 'tar -zxvf /vsts/bagent/vsts-agent-ubuntu.16.04-x64-2.111.1.tar.gz -C /vsts/bagent'
