74aab9f3c0e9dbbd22bcc474416a0813da95d29f
root <root@puppetmaster.haq5zuxiuwle5na4n2aml4hejd.ax.internal.cloudapp.net>
2017-01-26 09:15:12 +0000

    refactored

--- 01_puppet/puppet-templates/generateapp.sh
+++ 01_puppet/puppet-templates/generateapp.sh
@@ -2,13 +2,16 @@
 #
 # small helper app to guide your through configuring your template
 #
+echo "new name of file"
+read nfile
+cp buildagent-custom-template.pp $nfile 
 echo "what is the name of the resourcegroup of your puppetmaster"
 read rg
-sed  -i 's/msbf-demo/'$rg'/g' buildagent-custom.pp
+sed  -i 's/msbf-demo/'$rg'/g' $nfile
 echo "what is the name of your buildagent, ps think of a unique name (DNS)"
 read name
-sed  -i 's/buildagent1/'$name'/g' buildagent-custom.pp
+sed  -i 's/buildagent1/'$name'/g' $nfile
 echo "unique name of your storageaccount (only alfanumeric signs)"
 read sa
-sed  -i 's/cvugrinecdemo/'$sa'/g' buildagent-custom.pp
+sed  -i 's/cvugrinecdemo/'$sa'/g' $nfile
 
